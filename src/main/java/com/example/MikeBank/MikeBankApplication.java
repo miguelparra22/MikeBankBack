8e51ecd30c88854b4d09603b4bc52850ad810858
Miguel Angel <orjuelaparra@gmail.com>
2022-03-12 08:05:05 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/example/MikeBank/MikeBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.MikeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MikeBank/MikeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MikeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
